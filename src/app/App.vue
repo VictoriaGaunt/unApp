c2891f4e4572e85543837ae54aa12e501507a492
Victoria <vika.argent@gmail.com>
2025-04-29 19:18:42 +0500

    репозиторий на GitHub

--- src/app/App.vue
+++ src/app/App.vue
@@ -22,7 +22,7 @@ export default defineComponent({
         <router-view />
       </main>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="assets/vue.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="assets/vue.svg" class="logo vue" alt="Vue logo" />
